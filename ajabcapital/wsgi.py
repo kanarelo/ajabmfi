785be07092fef92b490627cfa169d01f299801b6
Mukewa Wekalao <kanarelo@gmail.com>
2016-02-10 21:53:56 +0300

    Adds wsgi for production

--- ajabcapital/wsgi.py
+++ ajabcapital/wsgi.py
@@ -11,6 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ajabcapital.settings")
+DJANGO_SETTINGS_MODULE = os.environ.get("DJANGO_SETTINGS_MODULE")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", 
+	(DJANGO_SETTINGS_MODULE or "ajabcapital.settings.production")
+)
 
 application = get_wsgi_application()
