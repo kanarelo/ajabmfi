6ea6f6895df4d0d1eed1021caa6a3492c66118d8
Mukewa Wekalao <kanarelo@gmail.com>
2016-02-18 11:23:58 +0300

    Adds financial accounting module

---
+++ ajabcapital/apps/financial_accounting/models.py
@@ -0,0 +1,42 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+'''
+10000000 | Assets
+11000000 | Cash and Cash Equivalents
+11200000 |      Cash at Bank
+11220000 |      Loan Fund Source
+1.1300.000 | Accounts Receivable
+1.1310.000 |        Interest Receivable
+1.1311.000 |        Penalty Receivable
+1.1312.000 |        Fee Receivable
+
+1.4000.000 | Net Loan Portfolio
+14100000 |      Gross Loan Portfolio(Loan Portfolio Control Account)
+14200000 |      Allowance for Loan Loss (contra account)
+14300000 |      Loans Recovered
+
+20000000 | Liabilities
+21000000 |      Deposit Protection Fund
+22000000 |      Accounts Payable
+22000000 |      Other Liabilities
+
+30000000 | Equity
+31000000 |      Owner's Equity
+32000000 |      Grants and Donations
+33000000 |      Retained Earnings
+
+40000000 | Income
+41000000 | Financial Income
+41100000 | Interest Income
+41200000 | Fee Income
+41210000 | Membership Fees
+41220000 | Loan Fees
+41230000 | Overdraft Fees
+41240000 | Penalty Fees
+
+50000000 | Expenses
+51000000 | Financial Expense
+51300000 | Write-Off Expense
+'''
