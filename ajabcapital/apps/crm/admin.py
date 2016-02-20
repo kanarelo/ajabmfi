57cd5410545474e194de553a9288f3766631995f
Mukewa Wekalao <kanarelo@gmail.com>
2016-02-20 10:12:33 +0300

    Adds CRM Models

--- ajabcapital/apps/crm/admin.py
+++ ajabcapital/apps/crm/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+models = globals().copy()
+
+for name, Model in models.iteritems():
+    if hasattr(Model, '_meta') and not Model._meta.abstract:
+        class Admin(admin.ModelAdmin):
+            model = Model
+
+            list_display = [f.name for f in Model._meta.fields[8:]]
+            list_filter = [f.name for f in Model._meta.fields[8:]]
+            list_per_page = 25
+
+        admin.site.register(Model, Admin)
